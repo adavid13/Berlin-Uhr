99d1401ca83beb1e3e1bf593c8bcbd7d1361628e
adavid13 <43451013+adavid13@users.noreply.github.com>
2020-07-12 21:34:29 -0300

    New features
    Added a top bar menu, with About, How to read? and Exit buttons.
    Created the About window that opens from the menu, which has a description of the Berlin Clock. As well, added a refresh method and button which exits and reopens the Python process for situations that the Python process was hanging (after computer hibernation for example) and the time is not correct.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from time import *
+import os
 
 try:
     # For Python 2
@@ -7,24 +8,62 @@ except ImportError:
     # For Python 3
     from tkinter import *
 
+"""
+Window and canvas config
+"""
 top = Tk()
+dead = False # Status flag
 top.geometry("450x450")
 top.title("Berlin Uhr")
 top.iconbitmap('appIcon.ico')
+top.resizable(height=FALSE, width=FALSE)
 can = Canvas(top, bg="#eee", height=450, width=450)
 can.pack()
+mainLabel = Label(top, text="Berlin Uhr (Set Theory Clock)", font=("Berlin Sans FB", 17))
+mainLabel.place(x=80, y=5)
 
+# Description text for About window
+description = 'Mengenlehreuhr (German for "Set Theory Clock") or\nBerlin-Uhr (German for "Berlin Clock"), ' \
+              'is the first\npublic clock in the world that tells the time by means\nof illuminated, coloured fields, ' \
+              'for which it entered the Guinness Book of Records upon its installation on\n17 June ' \
+              '1975.\n\nCommissioned by the Senate of Berlin and designed\nby Dieter Binninger, the original ' \
+              'full-sized\nMengenlehreuhr was originally located at the\nKurfürstendamm on the corner with ' \
+              'Uhlandstraße.\nAfter the Senate decommissioned it in 1995, the clock was relocated to a site in ' \
+              'Budapester Straße in front ofEuropa-Center, where it stands today.\n(Source: Wikipedia) '
+
+
+# Defining the About window
+def about_win():
+    about = Toplevel(top)
+    about.geometry("450x450")
+    about.resizable(height=FALSE, width=FALSE)
+    about_txt = Text(about, font=("Bahnschrift", 12), spacing1=4, spacing2=4)
+    about_txt.insert(INSERT, description)
+    about_txt['state'] = DISABLED
+    about_txt.place(x=25, y=25, width=400, height=350)
+    exit_btn = Button(about, text="Exit", command=about.destroy)
+    exit_btn.place(x=200, y=400)
+
+
+# Defining the menu bar
+bar = Menu(top)
+menu = Menu(bar, tearoff=0)
+menu.add_command(label="About", command=about_win)
+menu.add_command(label="How to read?", command=NO)
+menu.add_separator()
+menu.add_command(label="Exit", command=top.quit)
+bar.add_cascade(label="More", menu=menu)
+top.config(menu=bar)
+
+# Colour definitions
 outerCol = "#C0C0C0"  # outer silver colour for metal
 innerCol = "#777"  # inner grey colour
 orange = "#FFA500"  # orange colour for the lights
 red = "#E22D00"  # red colour for the lights
 
-mainLabel = Label(top, text="Berlin Uhr (Set Theory Clock)", font=("Arial", 17))
-mainLabel.place(x=75, y=5)
-
+# Shape creation
 ovalBack = can.create_oval(205, 55, 245, 95, fill=outerCol)
 innerOval = can.create_oval(210, 60, 240, 90, fill=innerCol)
-
 outerRec1 = can.create_rectangle(65, 115, 385, 165, fill=outerCol)
 innerRec1 = []
 startPt = 70
@@ -107,8 +146,18 @@ def paint():
         can.itemconfig(innerOval, fill=orange)
     else:
         can.itemconfig(innerOval, fill=innerCol)
-
     can.after(1000, paint)
+
+
+# Refresh method
+def refresh():
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+# Refresh button
+btnLabel2 = "Refresh"
+refreshButton2 = Button(top, text=btnLabel2, command=refresh)
+refreshButton2.place(x=200, y=390)
 
 
 paint()
